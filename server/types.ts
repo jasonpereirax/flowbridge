ae0081b66e5db3c1bcbaf01e0700cf902c7a01a3
Jason Pereira <82423694+jasonpereirax@users.noreply.github.com>
2026-02-24 11:18:42 -0300

    Delete server directory

--- server/types.ts
+++
@@ -1,9 +0,0 @@
-import type { Project, MacroNode, Screen } from '../src/types/index.js'
-
-export interface GenerationRequest {
-  project:  Project
-  dsNodes:  MacroNode[]
-  screens:  Screen[]
-  journeyId?: string
-  flowId?:    string
-}
